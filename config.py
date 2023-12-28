2724f15e257e507fd7f2d482d0ec51962096f490
urmila-empiric18 <up.empiric@gmail.com>
2023-12-28 15:32:51 +0530

    init6

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ import os
 from metagpt.const import PROJECT_ROOT
 from metagpt.logs import logger
 from metagpt.tools import SearchEngineType, WebBrowserEngineType
-# from metagpt.utils.singleton import Singleton
+from metagpt.utils.singleton import Singleton
 
 
 class NotConfiguredException(Exception):
